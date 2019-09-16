6dacd5dfb2a040dbc13fb4bc9a08d78857c67423
Ayaki-Matsuki <super.crou@gmail.com>
2019-09-16 23:13:43 +0900

    user.scssを編集　エラーメッセージの日本語化

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :phonenumber, presence: true
+  validates :address, presence: true
+  validates :name, presence: true
 end
