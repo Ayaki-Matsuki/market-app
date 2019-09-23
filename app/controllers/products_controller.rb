8e09411838eb5341d507ceb602466863c191065f
Ayaki-Matsuki <super.crou@gmail.com>
2019-09-23 20:51:08 +0900

    Edit product-show-html
    product-showviewの編集

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,5 +3,11 @@ class ProductsController < ApplicationController
   def index
     @products = Product.order('id ASC').limit(18)
   end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+
   
 end
